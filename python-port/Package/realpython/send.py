a26c61ad0ca7e030dff7bce85af89feb29097a53
Danc2050 <danc2@pdx.edu>
2020-03-25 21:17:35 -0700

    realpython implementation

---
+++ python-port/Package/realpython/send.py
@@ -0,0 +1,52 @@
+import email, smtplib, ssl, socket, sys
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send(SUBJECT=None, BODY=None, SENDER_EMAIL=None, PASSWORD=None, RECEIVER_EMAIL=None, CC=None, BCC=None, DATE=None, ATTACHMENTS=None, PRIORITY=None, SMTP=None, PORT=465):
+
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = SENDER_EMAIL
+    message["To"] = ", ".join(RECEIVER_EMAIL)
+    message["Cc"] = ", ".join(CC)
+   # message["Bcc"] = ", ".join(BCC) # TODO --
+    message["Date"] = DATE
+    message["Subject"] = SUBJECT
+    message["X-Priority"] = PRIORITY
+
+    # Add body. HTML will be preferentially attached, unless it fails; then plain will be used.
+    message.attach(MIMEText(BODY, "plain"))
+    message.attach(MIMEText(BODY, "html"))
+
+    # Add attachments - https://stackoverflow.com/questions/3362600/how-to-send-email-attachments
+    for path in ATTACHMENTS:
+        part = MIMEBase('application', "octet-stream")
+        with open(path, 'rb') as f1:
+            part.set_payload(f1.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition',
+                        'attachment; filename="{}"'.format(op.basename(path)))
+        message.attach(part)
+
+    # Log in to server using secure context and send email. Port 465 is for ssl (587 for starttls)
+    context = ssl.create_default_context()
+    try:
+        with smtplib.SMTP_SSL(SMTP, PORT, context=context) as server:
+        #with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(SENDER_EMAIL, PASSWORD)
+            server.sendmail(sender_email, [receiver_email] + BCC, message.as_string()) # BCC here keeps senders annoymous as we don't explicitly declare a header
+    except smtplib.SMTPHeloError as e:
+        print("Server did not reply. You may have Port 25 blocked on your host machine.")
+        sys.exit(2)
+    except smtplib.SMTPAuthenticationError as e:
+        print("Incorrect username/password combination. If you are using Google, you may need to lower the security settings (see the README.md for more information)")
+        sys.exit(2)
+    except smtplib.SMTPException as e:
+        print("Authentication failed")
+        sys.exit(2)
+    except Exception as error:
+        print(error)
+        sys.exit(2)
---
+++ python-port/Package/realpython/usage.py
@@ -0,0 +1,67 @@
+# Output usage
+# usage <program name>
+def usage():
+    print("Usage:  $1 <OPTION(S)>... -s <subject>\n"+
+    "or:     $1 <OPTION>\n"+
+    "One or more To, CC or BCC e-mail addresses are required. Send text messages by using the mobile providers e-mail to SMS or MMS gateway (https://en.wikipedia.org/wiki/SMS_gateway#Email_clients). All the options can also be set by opening the script in an editor and setting the variables at the top. See examples below.\n"+
+
+    "Options:\n"+
+        "-s, --subject    Subject\n"+
+                            "Escape sequences are expanded. Supports Unicode characters.\n"+
+        "-m --message      Message body\n"+
+                             "Escape sequences are expanded. Supports Unicode characters.\n"+
+        "-a --attachments Attachment filename\n"+
+                            "Use multiple times for multiple attachments. Supports Unicode characters in filename.\n"+
+        "-t --toemails    To e-mail address(es)\n"+
+        "-c --ccemails    CC e-mail address(es)\n"+
+        "-b --bccemails BCC e-mail address(es)\n"+
+        "-f --fromemail From e-mail address\n"+
+
+        "-S --smtp      SMTP server\n"+
+                            "Supported protocols: \"smtp\" and \"smtps\". Requires From e-mail address. Use \"smtp://localhost\" if running a mail server on this device.\n"+
+        "-u --username   SMTP server username\n"+
+        "-p --password   SMTP server password\n"+
+        "-P --priority   Priority\n"+
+                            "Supported priorities: \"5 (Lowest)\", \"4 (Low)\", \"Normal\", \"2 (High)\" and \"1 (Highest)\". Requires SMTP server.\n"+
+        "-C --certificate S/MIME Certificate filename for digitally signing the e-mails\n"+
+                            "It will ask you for the password the first time you run the script with this option. Requires SMTP server.\n"+
+        "-k --passphrase PGP secret key passphrase for digitally signing the e-mails with PGP/MIME\n"+
+                            "Requires SMTP server.\n"+
+        "-z --zipfile    Compress attachment(s) with zip\n"+
+        "-d --dryrun     Dry run, do not send the e-mail\n"+
+        "-V --verbose    Verbose, show the client-server communication\n"+
+                            "Requires SMTP server.\n"+
+
+        "-h --help       Display this help and exit\n"+
+        "-v --version    Output version information and exit\n"+
+
+    "Examples:\n"+
+        "Send e-mail\n"+
+        "$ $1 -s \"Example\" -t \"Example <example@example.com>\"\n"+
+
+        "Send e-mail with message\n"+
+        "$ $1 -s \"Example\" -m \"This is an example"'!'"\" -t \"Example <example@example.com>\"\n"+
+
+        "Send e-mail with message and single attachment\n"+
+        "$ $1 -s \"Example\" -m \"This is an example"'!'"\" -a example.txt -t \"Example <example@example.com>\"\n"+
+
+        "Send e-mail with message and multiple attachments\n"+
+        "$ $1 -s \"Example\" -m \"This is an example"'!'"\" -a example1.txt -a example2.txt -t \"Example <example@example.com>\"\n"+
+
+        "Send e-mail to a CC address\n"+
+        "$ $1 -s \"Example\" -t \"Example 1 <example1@example.com>\" -c \"Example 2 <example2@example.com>\"\n"+
+
+        "Send e-mail with a From address\n"+
+        "$ $1 -s \"Example\" -f \"Example <example@example.com>\" -t \"Example <example@example.com>\"\n"+
+
+        "Send e-mail with an external SMTP server\n"+
+        "$ $1 -s \"Example\" -f \"Example <example@example.com>\" -S \"smtps://mail.example.com\" -u \"example\" -p \"password\" -t \"Example <example@example.com>\"\n"+
+
+        "Send high priority e-mail\n"+
+        "$ $1 -s \"Example\" -f \"Example <example@example.com>\" -S \"smtps://mail.example.com\" -u \"example\" -p \"password\" -P \"1 (Highest)\" -t \"Example <example@example.com>\"\n"+
+
+        "Send e-mail digitally signed with an S/MIME Certificate\n"+
+        "$ $1 -s \"Example\" -f \"Example <example@example.com>\" -S \"smtps://mail.example.com\" -u \"example\" -p \"password\" -C \"cert.p12\" -t \"Example <example@example.com>\"\n"+
+
+        "Send e-mail digitally signed with PGP/MIME"+ "$ $1 -s \"Example\" -f \"Example <example@example.com>\" -S \"smtps://mail.example.com\" -u \"example\" -p \"password\" -k \"passphrase\" -t \"Example <example@example.com>\""+")\n")
+
