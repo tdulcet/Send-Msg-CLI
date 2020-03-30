63ad536ace0dd072c7e4efbd053e4bc3eaa27cfa
Danc2050 <danc2@pdx.edu>
2020-03-29 21:07:11 -0700

    finished configuration. Will restore README in next commit

--- python-port/Package/README.md
+++ python-port/Package/README.md
@@ -1,22 +1,26 @@
-# Python Port of Teal Dulcet's sendmsg.sh Script
+# Python3 port
 
-## Libraries to use (Suggestion credit: Teal Dulcet)
-[email](https://docs.python.org/3/library/email.html)
-[smptlib](https://docs.python.org/3/library/smtplib.html)
-[smime](https://pypi.org/project/smime/) -- get S/MIME encryption functionality. (alternatively [M2Crypto](https://tools.ietf.org/doc/python-m2crypto/howto.smime.html)).
+This is the Python port of the already excellent Bash Script.
 
-## Ways to Email Oneself
+I will use the PyPi package manager to upload this.
 
-# With script
+# Features
 
-output=$(sudo python2 runMythNew.py 2>&1); ./send.sh "'$HOSTNAME' is done"'!' "Exit code: $?\nOutput:\n$output\n"
-[comment]: # ( output=$(sudo python2 runMythNew.py 2>&1); ./send.sh "'$HOSTNAME' is done"'!' "Exit code: $?\nOutput:\n$output\n" )
+The features included in this script will not likely have all the 
+features contained in the bash script. However, the following features are
+necessary:
+1) email notification
+2) text notification
 
-# Without script 
+# Road Map
 
-output=$(sudo python2 runMythNew.py 2>&1); headers="From: Daniel Connelly <danc2@pdx.edu>\nTo: Daniel Connelly <5035044930@tmomail.net>\nSubject: =?utf-8?B?$(echo "'$HOSTNAME' is done"'!' | base64 -w 0)?=\nDate: $(date -R)\n"; message="Content-Type: text/plain; charset=utf-8\n\nExit code: $?\nOutput:\n$output\n"; echo -e "$headers$message" | curl -sS "smtps://smtp.gmail.com" --mail-from "danc2@pdx.edu" --mail-rcpt "5035044930@tmomail.net" -T - -u "danc2@pdx.edu:PASSWORD"
+~1) Make basic PyPi installation to get the idea of how it will work~
+ ~- follow these instructions: https://packaging.python.org/tutorials/packaging-projects/~
+2 or 3) Setup text and email notification
+ - follow these instructions: https://packaging.python.org/tutorials/packaging-projects/
+4) add advanced functionality.
+ - create a list of phone provider gateways to output to user.
+ - add other more advanced script features.
+ - compile to binary(?) and set a CLI command to the PATH variable so a 
+   user can use a CMDLine shortcut.
 
-
-
-# Replacing date -d command
-"Regarding the date -d command, I think you want to replace it with this function: https://docs.python.org/3/library/datetime.html#datetime.datetime.strptime, although you probably will want to use it directly on the output from openssl above, so that you can remove all the date commands..."
--- python-port/Package/realpython/send.py
+++ python-port/Package/realpython/send.py
@@ -6,20 +6,20 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def send(SUBJECT=None, BODY=None, SENDER_EMAIL=None, PASSWORD=None, RECEIVER_EMAIL=None, CC=None, BCC=None, DATE=None, ATTACHMENTS=None, PRIORITY=None, SMTP=None, VERBOSE=None, PORT=465):
+def send(SUBJECT=None, MESSAGE=None, USERNAME=None, PASSWORD=None, TOEMAILS=None, CC=None, BCC=None, DATE=None, ATTACHMENTS=None, PRIORITY=None, SMTP=None, VERBOSE=None, PORT=465):
 
     # Create a multipart message and set headers
     message = MIMEMultipart()
-    message["From"] = SENDER_EMAIL
-    message["To"] = ", ".join(RECEIVER_EMAIL)
+    message["From"] = USERNAME
+    message["To"] = ", ".join(TOEMAILS)
     message["Cc"] = ", ".join(CC)
     message["Date"] = DATE
     message["Subject"] = SUBJECT
     message["X-Priority"] = PRIORITY
 
-    # Add body. HTML will be preferentially attached, unless it fails; then plain will be used.
-    message.attach(MIMEText(BODY, "plain"))
-    message.attach(MIMEText(BODY, "html"))
+    # Add body.
+    #message.attach(MIMEText(MESSAGE, "plain"))
+    message.attach(MIMEText(MESSAGE, "html")) # send in HTML or plaintext?
 
     # Add attachments - https://stackoverflow.com/questions/3362600/how-to-send-email-attachments
     for path in ATTACHMENTS:
@@ -37,18 +37,17 @@ def send(SUBJECT=None, BODY=None, SENDER_EMAIL=None, PASSWORD=None, RECEIVER_EMA
         with smtplib.SMTP_SSL(SMTP, PORT, context=context) as server:
         #with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
             if VERBOSE:
-                print("TRUE")
                 server.set_debuglevel(2)
-            server.login(SENDER_EMAIL, PASSWORD)
-            server.sendmail(SENDER_EMAIL, [RECEIVER_EMAIL] + BCC, message.as_string()) # BCC here keeps senders annoymous as we don't explicitly declare a header
+            server.login(USERNAME, PASSWORD)
+            server.sendmail(USERNAME, [TOEMAILS] + BCC, message.as_string()) # BCC here keeps senders annoymous as we don't explicitly declare a header
     except smtplib.SMTPHeloError as e:
         print("Server did not reply. You may have Port 25 blocked on your host machine.")
         sys.exit(2)
     except smtplib.SMTPAuthenticationError as e:
-        print("Incorrect username/password combination. If you are using Google, you may need to lower the security settings (see the README.md for more information)")
+        print("Incorrect username/password combination or, if you are using Google, you may need to lower the security settings (see the README.md for more information).")
         sys.exit(2)
     except smtplib.SMTPException as e:
-        print("Authentication failed")
+        print("Authentication failed.")
         sys.exit(2)
     except Exception as error:
         import traceback
