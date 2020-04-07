379bad93978e25081d827e3a5c43110b2ad498d6
Danc2050 <danc2@pdx.edu>
2020-04-06 23:23:09 -0700

    all methods of sending a message work

--- python-port/Package/send.py
+++ python-port/Package/send.py
@@ -32,16 +32,26 @@ def set_main_headers(VARS, message):
     return message
     #message.__delitem__('Bcc') # remove Bcc area
 
+def attachments(message, attachments):
+    for path in attachments:
+        with open(path, 'rb') as f1:
+            part = MIMEApplication(
+                    f1.read(),
+                    name=op.basename(path))
+        #encoders.encode_base64(part)
+        part['Content-Disposition'] = 'attachment; filename="{}"'.format(op.basename(path))
+        del part["MIME-Version"]
+        message.attach(part)
+
 # thanks to: https://stackoverflow.com/questions/10496902/pgp-signing-multipart-e-mails-with-python
 def messageFromSignature(signature, content_type=None):
     message = EmailMessage()
-    #message.set_content(fmt.format(to_, from_, subject, msg))
     #message = Message()
     if content_type != None:
         message['Content-Type'] = content_type
     #signature.encode('ISO-8859-1')
     encoders.encode_7or8bit(message)
-    message.set_payload(signature)
+    message.set_payload(signature, charset='utf-8') # IMPORTANT: must specify charset to utf-8 to get signed body messages that have emojis in them (TODO: WHY?). But it does remove the string "This is an S/MIME signed message" in the header (not in the attachment).
     return message
 
 def send(VARS, FROMADDRESS, PORT=465):
@@ -50,90 +60,57 @@ def send(VARS, FROMADDRESS, PORT=465):
     # openssl cms -sign -signer "$CLIENTCERT" -in "file.txt"
 
     # SMIME
-    print("CERT: " + VARS["CERT"])
+    #print("CERT: " + VARS["CERT"])
     if VARS["CERT"]:
         cert_sig = subprocess.check_output("echo \""+VARS["MESSAGE"]+"\" | openssl cms -sign -signer "+VARS["CLIENTCERT"],shell=True).decode().strip("\n")
         # TODO -- add check for VARS["MESSAGE"] being None, here or elsewhere
-        #message = MIMEMessage(MIMEText(_text=cert_sig, _charset="utf-8"))
-        #message['Content-Disposition'] = 'attachment; filename="smime.p7s"'
-        #print(message)
-        #sys.exit()
-        #message = MIMEMessage(MIMEText(cert_sig,"plain"))
-        #signmsg = messageFromSignature(cert_sig)
-        #signmsg = messageFromSignature(cert_sig, 'text/plain')
-        #signmsg = messageFromSignature(cert_sig) # 'application/pkcs7-signature; name="smime.p7s"')
-        #print(cert_sig)
-        #sys.exit()
-        #message = MIMEMultipart(_subtype="mixed")
-        #message = MIMEMultipart(_subtype="signed", micalg="sha-256", protocol="application/pkcs7-signature")
-        #message = MIMEMultipart(_subtype="signed", micalg="sha-256") #protocol="application/pkcs7-signature")
-        #encoders.encode_7or8bit(message)
-        #message.attach(basemsg)
-        #message.attach(MIMEMessage(MIMEText(_text=cert_sig), "signed"))
-        mime_signed = MIMEText(_text=cert_sig, _subtype="plain")
-        #del mime_signed["MIME-Version"]
-        del mime_signed["MIME-Version"]
-        print(mime_signed.keys())
-
-        #print(mime_signed.replace_header(_name="MIME-Version", _value=""))
-        message = MIMEMessage(mime_signed)
-        del message["MIME-Version"]
-        #message.encode()
-        #message = MIMEMessage(MIMEText(_text=cert_sig), "plain")
+        basemsg = MIMEText(VARS["MESSAGE"])
+        del basemsg["MIME-Version"]
+        signmsg = messageFromSignature(cert_sig, 'application/pkcs7-signature; name="smime.p7s"')
+        signmsg['Content-Disposition'] = 'attachment; filename="smime.p7s"'
+        del signmsg["MIME-Version"]
+        message = MIMEMultipart(_subtype="signed", micalg="sha-256", protocol="application/pkcs7-signature")
+        attachments(message, VARS["ATTACHMENTS"])
+        set_main_headers(VARS, message)
+        message.attach(basemsg)
+        message.attach(signmsg)
 
     # PGP
     elif VARS["PASSPHRASE"]:
         pgp_sig = subprocess.check_output("echo \""+VARS["PASSPHRASE"]+"\" | gpg --pinentry-mode loopback --batch -o - -ab -u \""+FROMADDRESS+"\" --passphrase-fd 0", shell=True).decode().strip("\n")
-        basemsg = MIMEText(VARS["MESSAGE"])
+        basemsg = MIMEText(VARS["MESSAGE"], _charset="utf-8")
+        del basemsg["MIME-Version"]
         signmsg = messageFromSignature(pgp_sig, 'application/pgp-signature; name="signature.asc"')
         signmsg['Content-Disposition'] = 'attachment; filename="signature.asc"'
         message = MIMEMultipart(_subtype="signed", micalg="pgp-sha1", protocol="application/pgp-signature")
+        set_main_headers(VARS, message)
+        attachments(message, VARS["ATTACHMENTS"])
         message.attach(basemsg)
         message.attach(signmsg)
 
     # No signing of message
     else:
-        # Add attachments; thanks to: https://stackoverflow.com/questions/3362600/how-to-send-email-attachments
+        # Add attachments; thanks to Oli at https://stackoverflow.com/questions/3362600/how-to-send-email-attachments
+        mime_text = MIMEText(VARS["MESSAGE"], "plain")
+        del mime_text["MIME-Version"]
         if VARS["ATTACHMENTS"]:
             message = MIMEMultipart()
-            mime_text = MIMEText(VARS["MESSAGE"], "plain")
-            del mime_text["MIME-Version"]
             message.attach(mime_text)
 
             set_main_headers(VARS, message)
-
-
-            part = None
             for path in VARS["ATTACHMENTS"]:
-                print("HERE")
-                #part = Message()
-                #message = MIMEBase()
-                #part = MIMEBase('application', "octet-stream")
                 with open(path, 'rb') as f1:
-                    #part.set_payload(f1.read())
                     part = MIMEApplication(
                             f1.read(),
                             name=op.basename(path))
-               #     message.header.append(f1.read())
                 encoders.encode_base64(part)
-
                 part['Content-Disposition'] = 'attachment; filename="{}"'.format(op.basename(path))
-                #part.add_header('Content-Disposition',
-                #                'attachment; filename="{}"'.format(op.basename(path)))
                 message.attach(part)
-            #print(message.as_string())
-        #message = MIMEBase('application', "octet-stream", __text=message.as_string() + part.as_string())
-        # Regular message, no attachments
-        #mime_text = MIMEText(VARS["MESSAGE"], "plain")
-        #del mime_text["MIME-Version"]
-        if not VARS["ATTACHMENTS"]:
-            message = MIMEText(VARS["MESSAGE"],"plain")
 
         # Sending a message with an attachment requires Mutlipart()
-        else:
-            pass
-            #message = MIMEMultipart()
-            #message.attach(mime_text)
+        if not VARS["ATTACHMENTS"]:
+            message = mime_text
+            set_main_headers(VARS, message)
 
     # TODO -- try this https://stackoverflow.com/questions/10496902/pgp-signing-multipart-e-mails-with-python
 #    gpg = gnupg.GPG()
@@ -156,7 +133,7 @@ def send(VARS, FROMADDRESS, PORT=465):
                 server.login(VARS["USERNAME"], VARS["PASSWORD"])
                 server.send_message(message) # send_message() annoymizes BCC, rather than sendmail().
                 #server.sendmail(VARS["FROMEMAIL"], VARS["TOEMAILS"], message.as_string()) # send_message() annoymizes BCC, rather than sendmail().
-                print("Message sent")
+                #print("Message sent")
     except smtplib.SMTPHeloError as e:
         print("Server did not reply. You may have Port 25 blocked on your host machine.")
         sys.exit(2)
